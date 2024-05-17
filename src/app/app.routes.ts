d37ac9bc4bc86c312f178cc11c01c6e563652b40
semjasa83 <marceldezanna@yahoo.de>
2024-05-17 23:36:11 +0200

    header styled and components insert

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import {  HomeComponent } from "./content/home/home.component";
+import {  AboutComponent } from "./content/about/about.component";
+import {  ProjectsComponent } from "./content/projects/projects.component";
+import {  ContactComponent } from "./content/contact/contact.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'contact', component: ContactComponent },
+
+  { path: '**', redirectTo: '404' }
+];
