9bd7a7bb0a21a5bab8563aa044147f2f7afcdf7b
semjasa83 <marceldezanna@yahoo.de>
2024-05-19 01:10:02 +0200

    sidebar style finish

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../utility/header/header.component";
+import { SidebarComponent } from "../utility/sidebar/sidebar.component";
+import { FooterComponent } from "../utility/footer/footer.component";
+import { RouterOutlet } from '@angular/router';
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
     imports: [
-        HeaderComponent
+        HeaderComponent,
+        SidebarComponent,
+        FooterComponent,
+        RouterOutlet
     ]
 })
 export class HomeComponent {
+
+    
+
 
 }
