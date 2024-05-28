f5285df3e4dbb9ff9c8a1a60feb33f20ada9169a
semjasa83 <marceldezanna@yahoo.de>
2024-05-29 01:27:21 +0200

    rework, header and explorer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,14 @@
-import { Routes } from '@angular/router';
-import { AboutComponent } from "./content/about/about.component";
-import { ProjectsComponent } from "./content/projects/projects.component";
-import { ContactComponent } from "./content/contact/contact.component";
-import { HomeComponent } from './home/home.component';
-import { HelloComponent } from './content/hello/hello.component';
+import {Routes} from '@angular/router';
+import {AboutComponent} from "./content/about/about.component";
+import {ProjectsComponent} from "./content/projects/projects.component";
+import {ContactComponent} from "./content/contact/contact.component";
+import {HelloComponent} from './content/hello/hello.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent,
-    children: [
-      { path: 'hello', component: HelloComponent },
-      { path: 'about', component: AboutComponent },
-      { path: 'projects', component: ProjectsComponent },
-      { path: 'contact', component: ContactComponent },
-    ]
-  },
-  { path: '**', redirectTo: '404' }
+  {path: '', redirectTo: '', pathMatch: 'full'},
+  {path: 'hello', component: HelloComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'projects', component: ProjectsComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: '**', redirectTo: '404'}
 ];
--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import {ExplorerComponent} from "../utility/explorer/explorer.component";
+import {FooterComponent} from "../utility/footer/footer.component";
+import {HeaderComponent} from "../utility/header/header.component";
+import {RouterOutlet} from "@angular/router";
+import {SidebarComponent} from "../utility/sidebar/sidebar.component";
 
 @Component({
   selector: 'app-content',
   standalone: true,
-  imports: [],
+    imports: [
+        ExplorerComponent,
+        FooterComponent,
+        HeaderComponent,
+        RouterOutlet,
+        SidebarComponent
+    ],
   templateUrl: './content.component.html',
   styleUrl: './content.component.scss'
 })
