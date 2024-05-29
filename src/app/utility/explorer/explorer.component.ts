5f4f6a713b6ee06556267399dadbd19a3a583f19
semjasa83 <marceldezanna@yahoo.de>
2024-05-30 00:20:56 +0200

    routerlinks and active state implemented

--- src/app/utility/explorer/explorer.component.ts
+++ src/app/utility/explorer/explorer.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from "@angular/common";
+import {RouterLink, RouterLinkActive} from "@angular/router";
 
 @Component({
   selector: 'app-explorer',
   standalone: true,
-  imports: [],
+    imports: [
+        NgOptimizedImage,
+        RouterLink,
+        RouterLinkActive
+    ],
   templateUrl: './explorer.component.html',
   styleUrl: './explorer.component.scss'
 })
