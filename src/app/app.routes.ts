60ff6e1c99ea00b79425f0053cd752838bf4e3c7
semjasa83 <marceldezanna@yahoo.de>
2024-06-01 00:06:17 +0200

    hello to homecomponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,11 @@ import {Routes} from '@angular/router';
 import {AboutComponent} from "./content/about/about.component";
 import {ProjectsComponent} from "./content/projects/projects.component";
 import {ContactComponent} from "./content/contact/contact.component";
-import {HelloComponent} from './content/hello/hello.component';
+import {HomeComponent} from "./content/home/home.component";
 
 export const routes: Routes = [
   {path: '', redirectTo: '', pathMatch: 'full'},
-  {path: 'hello', component: HelloComponent},
+  {path: 'home', component: HomeComponent},
   {path: 'about', component: AboutComponent},
   {path: 'projects', component: ProjectsComponent},
   {path: 'contact', component: ContactComponent},
