c0467671d7a009147c166533806aeb4e76eada05
semjasa83 <marceldezanna@yahoo.de>
2024-05-31 23:31:24 +0200

    changes on interface

--- src/app/content/hello/hello.component.ts
+++ src/app/content/hello/hello.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {NgFor} from "@angular/common";
 
 @Component({
   selector: 'app-hello',
   standalone: true,
-  imports: [],
+  imports: [
+    NgFor
+  ],
   templateUrl: './hello.component.html',
   styleUrl: './hello.component.scss'
 })
