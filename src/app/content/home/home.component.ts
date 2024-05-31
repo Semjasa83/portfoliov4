769b85e4240b58e01876c0ebc649d0f103738b27
semjasa83 <marceldezanna@yahoo.de>
2024-06-01 00:59:08 +0200

    sidebar and explorer fixes scss

--- src/app/content/home/home.component.ts
+++ src/app/content/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { NgFor } from "@angular/common";
+import { TranslateModule } from "@ngx-translate/core";
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    NgFor
+    NgFor,
+    TranslateModule
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
