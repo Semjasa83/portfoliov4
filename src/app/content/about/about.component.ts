d35ae5be760f0035327808a6d7137517f0f2ae2c
semjasa83 <marceldezanna@yahoo.de>
2024-06-06 00:47:09 +0200

    fix on settings button and scrollbar

--- src/app/content/about/about.component.ts
+++ src/app/content/about/about.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { BackgroundComponent } from '../../utility/background/background.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [
+    BackgroundComponent,
+    TranslateModule
+  ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
