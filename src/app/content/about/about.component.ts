467dcae066501eb2895daf134a89502969ca122f
semjasa83 <marceldezanna@yahoo.de>
2024-06-07 00:16:03 +0200

    rework on picture at aboutme

--- src/app/content/about/about.component.ts
+++ src/app/content/about/about.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { BackgroundComponent } from '../../utility/background/background.component';
 import { TranslateModule } from '@ngx-translate/core';
+import { NgOptimizedImage } from "@angular/common";
 
 @Component({
   selector: 'app-about',
   standalone: true,
   imports: [
     BackgroundComponent,
-    TranslateModule
+    TranslateModule,
+    NgOptimizedImage
   ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
