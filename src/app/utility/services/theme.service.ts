7386d6fc59aea1292028861e33e5b66857c0801a
semjasa83 <marceldezanna@yahoo.de>
2024-06-09 23:39:50 +0200

    theme service and scss fixes for content wrapper

---
+++ src/app/utility/services/theme.service.ts
@@ -0,0 +1,14 @@
+import { Injectable, Renderer2, RendererFactory2 } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ThemeService {
+
+  constructor() {
+
+  }
+
+}
