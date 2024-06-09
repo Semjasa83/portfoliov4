77030bc16e89da8d2f6eb3147b0b26c33ceb4d4c
semjasa83 <marceldezanna@yahoo.de>
2024-06-10 01:33:18 +0200

    theme service toggle doesnt work properly

--- src/app/content/settings/settings.component.ts
+++ src/app/content/settings/settings.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { ThemeService } from '../../utility/services/theme/theme.service';
+import { Theme } from '../../../assets/themes/theme_interface';
+import { ButtonComponent } from "../../utility/button/button.component";
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
-  selector: 'app-settings',
-  standalone: true,
-  imports: [],
-  templateUrl: './settings.component.html',
-  styleUrl: './settings.component.scss'
+    selector: 'app-settings',
+    standalone: true,
+    templateUrl: './settings.component.html',
+    styleUrl: './settings.component.scss',
+    imports: [
+      ButtonComponent,
+      TranslateModule
+    ]
 })
 export class SettingsComponent {
 
-}
+  public isAyuThemeActive: boolean = true;
+
+  constructor(private themeService: ThemeService) { }
+
+  public toggleTheme(): void {
+    this.isAyuThemeActive = !this.isAyuThemeActive;
+    if (this.isAyuThemeActive) {
+      this.themeService.setTheme(Theme.Ayu);
+    } else {
+      this.themeService.setTheme(Theme.Test);
+    }
+
+}}
---
+++ src/app/utility/services/theme/theme.service.ts
@@ -0,0 +1,31 @@
+import { EventEmitter, Injectable, Renderer2, RendererFactory2 } from '@angular/core';
+import { IThemeOptions, Theme } from '../../../../assets/themes/theme_interface';
+import { ayuTheme, testTheme } from '../../../../assets/themes/theme_classes';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ThemeService {
+
+  constructor() {}
+
+  public themeChange: EventEmitter<IThemeOptions> = new EventEmitter<IThemeOptions>();
+
+  private themes: Array<IThemeOptions> = [ayuTheme, testTheme];
+  private activeTheme: Theme = Theme.Ayu;
+
+  public getActiveTheme(): IThemeOptions {
+      const theme: IThemeOptions | undefined = this.themes.find((option: IThemeOptions) => option.name === this.activeTheme);
+      if (!theme) {
+          throw new Error(`Theme not found: '${this.activeTheme}'`);
+      }
+
+      return theme;
+  }
+
+  public setTheme(name: Theme): void {
+      this.activeTheme = name;
+      this.themeChange.emit( this.getActiveTheme());
+  }
+}
