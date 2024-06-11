cfd74fbf5821703c7933309ac83161e4ddcc6627
semjasa83 <marceldezanna@yahoo.de>
2024-06-12 00:25:49 +0200

    files prepared

--- src/app/utility/services/theme/theme.service.ts
+++ src/app/utility/services/theme/theme.service.ts
@@ -1,12 +1,25 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Renderer2, RendererFactory2 } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ThemeService {
+  private renderer: Renderer2;
 
-  constructor() { }
+  constructor(private rendererFactory: RendererFactory2) {
+    this.renderer = this.rendererFactory.createRenderer(null, null);
+   }
 
-  
+   setTheme(theme: { [key: string]: string }): void {
+    Object.keys(theme).forEach(key => {
+      this.renderer.setStyle(document.body, `--${key}`, theme[key]);
+    });
+  }
 }
+
+// in der componente als merker
+// this.themeService.setTheme({
+//   'primary-color': '#ff0000',
+//   'secondary-color': '#00ff00',
+// });
 
