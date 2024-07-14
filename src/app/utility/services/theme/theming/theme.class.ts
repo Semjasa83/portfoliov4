6a28c932bf7e7b81cca649e71ae79f2f60a9214a
semjasa83 <marceldezanna@yahoo.de>
2024-07-14 21:24:15 +0200

    unleashed, ayu, vscode done

--- src/app/utility/services/theme/theming/theme.class.ts
+++ src/app/utility/services/theme/theming/theme.class.ts
@@ -1,23 +1,3 @@
-// import { AvailableProperties, Theme } from './theme.interface';
-
-// export const themes: IThemeOptions[] = [
-// {
-//     name: Theme.Ayu,
-//     customProperties: {
-//         [AvailableProperties.Background]: '#0d1017',
-//         [AvailableProperties.BackgroundContrast]: '#bfbdb6',
-//     }
-// },
-
-// {
-//     name: Theme.Test,
-//     customProperties: {
-//         [AvailableProperties.Background]: '#263238',
-//         [AvailableProperties.BackgroundContrast]: '#c9d1c9',
-//     }
-// }
-// ]
-
 
 export const ayu = {
     '--primary-color': '#aad94c',
@@ -117,9 +97,108 @@ export const vscode = {
 
     '--button-hover-color': '#0d1017',
     '--button-hover-contrast-color': '#bfbdb6',
+    '--button-txt-hover-color': '#ffffff',
+    '--button-txt-hover-contrast-color': '#0d1017'
+}
+
+export const nightowl = {
+    '--primary-color': '#034072',
+    '--primary-contrast-color': '#0d1017',
+    '--primary-txt-color': '#aad94c',
+    '--primary-txt-contrast-color': '#0d1017',
+
+    '--secondary-color': '#034072',
+    '--secondary-contrast-color': '#0d1017',
+    '--secondary-txt-color': '#034072',
+    '--secondary-txt-contrast-color': '#0d1017',
+
+    '--surface-color': '#011627',
+    '--surface-contrast-color': '#d6deeb',
+    '--surface-contrast-transparent-color': '#5f7e97',
+    '--surface-txt-color': '#131822',
+    '--surface-txt-contrast-color': '#bfbdb6',
+    '--surface-txt-contrast-transparent-color': '#bfbdb644',
+
+    '--background-color': '#011627',
+    '--background-contrast-color': '#d6deeb',
+    '--background-txt-color': '#0d1017',
+    '--background-txt-contrast-color': '#bfbdb6',
+
+    '--success-color': '#7fd962',
+    '--success-contrast-color': '#0d1017',
+    '--success-txt-color': '#7fd962',
+    '--success-txt-contrast-color': '#0d1017',
+
+    '--warning-color': '#ffb454',
+    '--warning-contrast-color': '#0d1017',
+    '--warning-txt-color': '#ffb454',
+    '--warning-txt-contrast-color': '#0d1017',
+
+    '--error-color': '#f26d78',
+    '--error-contrast-color': '#0d1017',
+    '--error-txt-color': '#f26d78',
+    '--error-txt-contrast-color': '#0d1017',
+
+    '--secondary-pulse-color': '#39bae6aa',
+
+    '--button-color': '#aad94c',
+    '--button-contrast-color': '#0d1017',
+    '--button-txt-color': '#aad94c',
+    '--button-txt-contrast-color': '#0d1017',
+
+    '--button-hover-color': '#0d1017',
+    '--button-hover-contrast-color': '#bfbdb6',
     '--button-txt-hover-color': '#bfbdb6',
     '--button-txt-hover-contrast-color': '#0d1017'
 }
 
-export const nightowl = {}
-export const unleashed = {}
+export const unleashed = {
+    '--primary-color': '#FF6E48',
+    '--primary-contrast-color': '#9FA9BD',
+    '--primary-txt-color': '#00EDFF',
+    '--primary-txt-contrast-color': '#9FA9BD',
+
+    '--secondary-color': '#39bae6',
+    '--secondary-contrast-color': '#9FA9BD',
+    '--secondary-txt-color': '#39bae6',
+    '--secondary-txt-contrast-color': '#9FA9BD',
+
+    '--surface-color': '#161A21',
+    '--surface-contrast-color': '#9FA9BD',
+    '--surface-contrast-transparent-color': '#9FA9BD44',
+    '--surface-txt-color': '#FF6E48',
+    '--surface-txt-contrast-color': '#00EDFF',
+    '--surface-txt-contrast-transparent-color': '#9FA9BD44',
+
+    '--background-color': '#13161B',
+    '--background-contrast-color': '#9FA9BD',
+    '--background-txt-color': '#1B1C21',
+    '--background-txt-contrast-color': '#9FA9BD',
+
+    '--success-color': '#7fd962',
+    '--success-contrast-color': '#1B1C21',
+    '--success-txt-color': '#7fd962',
+    '--success-txt-contrast-color': '#1B1C21',
+
+    '--warning-color': '#ffb454',
+    '--warning-contrast-color': '#1B1C21',
+    '--warning-txt-color': '#ffb454',
+    '--warning-txt-contrast-color': '#1B1C21',
+
+    '--error-color': '#f26d78',
+    '--error-contrast-color': '#1B1C21',
+    '--error-txt-color': '#f26d78',
+    '--error-txt-contrast-color': '#1B1C21',
+
+    '--secondary-pulse-color': '#39bae6aa',
+
+    '--button-color': '#FF6E48',
+    '--button-contrast-color': '#1B1C21',
+    '--button-txt-color': '#FF6E48',
+    '--button-txt-contrast-color': '#1B1C21',
+
+    '--button-hover-color': '#1B1C21',
+    '--button-hover-contrast-color': '#39bae6',
+    '--button-txt-hover-color': '#39bae6',
+    '--button-txt-hover-contrast-color': '#1B1C21'
+}
